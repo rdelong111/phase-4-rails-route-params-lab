a7a72dfeefa0eb48fc82937feb13eec184548649
Ryan DeLong <rdelong111@gmail.com>
2022-12-18 10:20:40 -0600

    commit tree 47b53df11e5a6ac005c6c6c5811efc6ee65bc1f9

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,15 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+    if params[:name]
+      name = params[:name].titleize
+      students = Student.where("first_name = ? OR last_name = ?", name, name).all
+    end
     render json: students
+  end
+
+  def find_student
+    render json: Student.find(params[:id])
   end
 
 end
